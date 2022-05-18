5b5052a9a3e6905a2da531081f2b9da40982339e
Emmanuel S Sogelola <>
2022-05-18 05:30:28 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+from inventory import app
+
+
+
+if __name__ == "__main__":  # Makes sure this is the main process
+	app.run( # Starts the site
+		host='0.0.0.0',  # EStablishes the host, required for repl to detect the site
+		port=80
+	)
